4372d168b9c7e8700defa47a5b0235a0b1fc56d5
Tanvir Islam <mdtanvirislams@gmail.com>
2024-03-22 16:54:36 +1100

    initilize basic functionality

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            return View();
+        }
+    }
+}
