1c3191d5ee948aa3b70e09523386ec54bd7290b2
Tanvir Islam <mdtanvirislams@gmail.com>
2024-03-25 02:22:15 +1100

    upsert part2 with image file

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -40,6 +40,7 @@ namespace Bulky.Models
         [ForeignKey("CategoryId")]
         [ValidateNever]
         public Category Category { get; set; }
+        [ValidateNever]
         public string ImageUrl { get; set; }
 
 
