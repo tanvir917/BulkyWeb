e961410e670f8e73a7736747f83eba0832751771
Tanvir Islam <mdtanvirislams@gmail.com>
2024-03-24 14:43:07 +1100

    adding razor project

---
+++ BulkyWebRazor_temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using BulkyWebRazor_temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "History", DisplayOrder = 2 }
+            );
+        }
+    }
+}
+
