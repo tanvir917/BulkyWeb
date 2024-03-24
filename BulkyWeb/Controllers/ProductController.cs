71aa86ea1829b34eda13cb7d01920f36e5ed7d31
Tanvir Islam <mdtanvirislams@gmail.com>
2024-03-25 01:33:25 +1100

    upsert implementation

--- BulkyWeb/Controllers/ProductController.cs
+++ BulkyWeb/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace BulkyWeb.Controllers
 
             return View(objProductList);
         }
-        public IActionResult Create()
+        public IActionResult Upsert(int? id) //update and insert
         {
             IEnumerable<SelectListItem> categoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
             {
@@ -35,10 +35,20 @@ namespace BulkyWeb.Controllers
                 CategoryList = categoryList,
                 Product = new Product()
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                //update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+
         }
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -56,34 +66,6 @@ namespace BulkyWeb.Controllers
                 });
                 return View(productVM);
             }
-        }
-
-        public IActionResult Edit(int id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id); //only works with primary key
-            //Product productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            //Product productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product edited successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
         }
 
         public IActionResult Delete(int? id)
