10820d293d602821216526b4b78c30fd698d5b8c
Tanvir Islam <mdtanvirislams@gmail.com>
2024-04-17 00:01:09 +1000

    adding role management

---
+++ Bulky.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bulky.Models.ViewModels
+{
+    public class RoleManagementVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+
+    }
+}
+
--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
@@ -18,11 +19,13 @@ namespace BulkyWeb.Areas.Admin.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public UserController(IUnitOfWork unitOfWork, ApplicationDbContext db)
+        public UserController(IUnitOfWork unitOfWork, ApplicationDbContext db, UserManager<IdentityUser> userManager)
         {
             _unitOfWork = unitOfWork;
             _db = db;
+            _userManager = userManager;
         }
         // GET: CompanyController
         public ActionResult Index()
@@ -30,6 +33,52 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult RoleManagement(string userId)
+        {
+            string RoleID = _db.UserRoles?.FirstOrDefault(u => u.UserId == userId)?.RoleId;
+
+            RoleManagementVM RoleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            string RoleID = _db.UserRoles?.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id)?.RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            if (!(roleManagementVM.ApplicationUser.Role == oldRole))
+            {
+                //a role was updated
+                ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+                if (roleManagementVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+            }
+            return RedirectToAction(nameof(Index));
+        }
         [HttpGet]
         public IActionResult GetAll()
         {
